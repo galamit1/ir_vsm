4e7a4006bf10a4d9a6f3ef470cd5d4372046975c
Gal Amit <agal@agal-a02.vmware.com>
2022-07-18 11:58:16 +0300

    add IR class

--- vsm_ir.py
+++ vsm_ir.py
@@ -12,120 +12,128 @@ import ssl
 INVERTED_INDEX_PATH = "vsm_inverted_index.json"
 OUTPUT_PATH = "ranked_query_docs.txt"
 
-# bm25 constants
-K = 1.2
-B = 0.75
 
-# This dictionary holds all the words, the tf-idf for each file for every word.
-dict_tf_idf_scores = {}
-words_per_file = {}
-query_dictionary = {}
-document_reference_length = {}
-corpus = {}
+class IR(object):
+    # bm25 constants
+    K = 1.2
+    B = 0.75
 
-# part 1: calculate tf-idf scores
+    def __init__(self):
+        # inverted index
+        self.dict_tf_idf_scores = {}
+        self.words_per_file = {}
+        self.query_dictionary = {}
+        self.document_reference_length = {}
+        self.corpus = {}
 
-
-def parse_file(filename):
-    try: #TODO remove this block
-        _create_unverified_https_context = ssl._create_unverified_context
-    except AttributeError:
-        pass
-    else:
-        ssl._create_default_https_context = _create_unverified_https_context
-
-    nltk.download('stopwords')
-    stop_words = set(stopwords.words("english"))
-
-    tokenizer = RegexpTokenizer(r'\w+')
-    ps = PorterStemmer()
-    xml_tree = ET.parse(filename)
-    root = xml_tree.getroot()
-
-    for child in root.findall("./RECORD"):
-        record_id = 0
-        text = ""
-        for entry in child:
-            if entry.tag == "TITLE" or entry.tag == "EXTRACT" or entry.tag == "ABSTRACT":
-                text += str(entry.text) + " "
-            if entry.tag == "RECORDNUM":
-                record_id = int(entry.text)
-                if record_id not in document_reference_length:
-                    document_reference_length[record_id] = 0
-
-        text = tokenizer.tokenize(text.lower())  # tokens
-        filtered_text = [ps.stem(word) for word in text if word not in stop_words]  # stopwords + stem
-
-        update_dictionary(filtered_text, record_id)
-        words_per_file[record_id] = len(text)
-
-
-def update_dictionary(text, file_name):
-    for word in text:
+        # nltk classes
         try:
-            if dict_tf_idf_scores.get(word).get(file_name):
-                dict_tf_idf_scores[word][file_name]["count"] += 1
-        except:
-            dict_tf_idf_scores[word] = {file_name: {"count": 1, "tf_idf": 0}}
+            nltk.download('stopwords')
+        except:  # disable SSL check. reference: https://stackoverflow.com/questions/38916452/nltk-download-ssl-certificate-verify-failed
+            try:
+                _create_unverified_https_context = ssl._create_unverified_context
+            except AttributeError:
+                pass
+            else:
+                ssl._create_default_https_context = _create_unverified_https_context
+            nltk.download('stopwords')
+        self.stop_words = set(stopwords.words("english"))
+        self.tokenizer = RegexpTokenizer(r'\w+')
+        self.ps = PorterStemmer()
 
+    # part 1: calculate tf-idf scores
 
-def calc_tf_idf_score(docs_count, avgdl):
-    for word in dict_tf_idf_scores:
-        for file in dict_tf_idf_scores[word]:
-            word_frequency = dict_tf_idf_scores[word][file].get('count')
+    def parse_file(self, filename):
+        xml_tree = ET.parse(filename)
+        root = xml_tree.getroot()
 
-            # compute tf_idf
-            tf = word_frequency / words_per_file.get(file)
-            idf = np.log2(docs_count / len(dict_tf_idf_scores[word]))
-            dict_tf_idf_scores[word][file]["tf_idf"] = tf * idf
-            document_reference_length[file] += (tf * idf) ** 2
+        for child in root.findall("./RECORD"):
+            record_id = 0
+            text = ""
+            for entry in child:
+                if entry.tag == "TITLE" or entry.tag == "EXTRACT" or entry.tag == "ABSTRACT":
+                    text += str(entry.text) + " "
+                if entry.tag == "RECORDNUM":
+                    record_id = int(entry.text)
+                    if record_id not in self.document_reference_length:
+                        self.document_reference_length[record_id] = 0
 
-            # compute bm25
-            bm25 = (idf * word_frequency * (K + 1)) / \
-                   (word_frequency + K * (1 - B + B * words_per_file.get(file) / avgdl))
-            dict_tf_idf_scores[word][file]["bm25"] = bm25
+            text = self.tokenizer.tokenize(text.lower())  # tokens
+            filtered_text = [self.ps.stem(word) for word in text if word not in self.stop_words]  # stopwords + stem
 
+            self.update_dictionary(filtered_text, record_id)
+            self.words_per_file[record_id] = len(text)
 
-def create_mapping(xml_dir_path):
-    [parse_file(xml_dir_path + "/" + file_name) if file_name.endswith(".xml") else None for file_name in
-     os.listdir(xml_dir_path)]
+    def update_dictionary(self, text, file_name):
+        for word in text:
+            if self.dict_tf_idf_scores.get(word, {}).get(file_name):
+                self.dict_tf_idf_scores[word][file_name]["count"] += 1
+            else:
+                self.dict_tf_idf_scores[word] = {file_name: {"count": 1, "tf_idf": 0}}
 
-    amount_of_docs = len(document_reference_length)
-    avgdl = sum(words_per_file.values()) / amount_of_docs
-    calc_tf_idf_score(amount_of_docs, avgdl)
+    def calc_tf_idf_score(self, docs_count, avgdl):
+        for word in self.dict_tf_idf_scores:
+            for file in self.dict_tf_idf_scores[word]:
+                word_frequency = self.dict_tf_idf_scores[word][file].get('count')
 
-    # norm
-    for file in document_reference_length:
-        document_reference_length[file] = np.sqrt(document_reference_length[file])
+                # compute tf_idf
+                tf = word_frequency / self.words_per_file.get(file)
+                idf = np.log2(docs_count / len(self.dict_tf_idf_scores[word]))
+                self.dict_tf_idf_scores[word][file]["tf_idf"] = tf * idf
+                self.document_reference_length[file] += (tf * idf) ** 2
 
-    # add new dict to corpus
-    corpus["dictionary"] = dict_tf_idf_scores
-    corpus["document_reference"] = document_reference_length
+                # compute bm25
+                bm25 = (idf * word_frequency * (self.K + 1)) / \
+                       (word_frequency + self.K * (1 - self.B + self.B * self.words_per_file.get(file) / avgdl))
+                self.dict_tf_idf_scores[word][file]["bm25"] = bm25
 
-    with open(INVERTED_INDEX_PATH, "w") as inverted_index_file:
-        json.dump(corpus, inverted_index_file)
+    def create_mapping(self, xml_dir_path):
+        [self.parse_file(xml_dir_path + "/" + file_name) if file_name.endswith(".xml") else None for file_name in
+         os.listdir(xml_dir_path)]
 
+        amount_of_docs = len(self.document_reference_length)
+        avgdl = sum(self.words_per_file.values()) / amount_of_docs
+        self.calc_tf_idf_score(amount_of_docs, avgdl)
 
-### PART 2: Information Retrieval given a query. ###
+        # norm
+        for file in self.document_reference_length:
+            self.document_reference_length[file] = np.sqrt(self.document_reference_length[file])
 
-def query(ranking_func, index_path, question):
-    with open(index_path, "r") as inverted_index_file:
-        corpus = json.load(inverted_index_file)
+        # add new dict to corpus
+        corpus = {"dictionary": self.dict_tf_idf_scores,
+                  "document_reference": self.document_reference_length}
 
-    dict_tf_idf_scores.update(corpus["dictionary"])
-    document_reference_length.update(corpus["document_reference"])
+        with open(INVERTED_INDEX_PATH, "w") as inverted_index_file:
+            json.dump(corpus, inverted_index_file)
+
+    ### PART 2: Information Retrieval given a query. ###
+
+    def load_ir(self, index_path):
+        with open(index_path, "r") as inverted_index_file:
+            corpus = json.load(inverted_index_file)
+
+        self.dict_tf_idf_scores = corpus["dictionary"]
+        self.document_reference_length = corpus["document_reference"]
+
+    def normalize_query(self, query):
+        query = self.tokenizer.tokenize(query.lower())  # tokens
+        return [self.ps.stem(word) for word in query if word not in self.stop_words]  # stopwords + stem
+
+    def perform_query(self, ranking_func, index_path, query):
+        self.load_ir(index_path)
+        query = self.normalize_query(query)
 
 
 def main():
     # call methods based on system arguments
+    ir = IR()
     try:
         if sys.argv[1] == 'create_index':
-            return create_mapping(sys.argv[2])
+            return ir.create_mapping(sys.argv[2])
         elif sys.argv[1] == 'query':
-            return query(*sys.argv[2:5])
-    except ValueError:
+            return ir.perform_query(*sys.argv[2:5])
+    except IndexError:
         print("Wrong Input")
-
 
 
 if __name__ == '__main__':
